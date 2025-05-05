09ea1dec97c723c85c85d5432ae4374dc193dc4c
ufuk <95375222+utblr@users.noreply.github.com>
2025-05-05 12:32:55 +0300

    Update particles.py

--- particles.py
+++ particles.py
@@ -4,10 +4,13 @@ positions = np.loadtxt("positions.txt", delimiter=",") # m
 velocities = np.loadtxt("velocities.txt", delimiter=",") # m
 masses = np.loadtxt("masses.txt", delimiter=",") # kg
 
+
 g = np.array([0, -9.81]) # kg/m-s2
 
 # time increment between the consecutive time steps
 dt = 0.01 # s
+
+old_positions = np.copy(positions)
 
 # simulate forces, velocities, and positions for 100 time steps
 for step in range(100):
